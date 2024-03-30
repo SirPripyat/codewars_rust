36bbbbc347f70115903f5a22da5f2d88849e000d
leonardorossi <08.leo.rossi@gmail.com>
2024-03-30 17:35:09 -0300

    exclude target

--- target/debug/deps/codewars_rust-5a4ffc11e18e4e7f.d
+++
@@ -1,9 +0,0 @@
-/Users/leonardorossi/Desktop/projetos/codewars-rust/target/debug/deps/codewars_rust-5a4ffc11e18e4e7f: src/main.rs src/cockroach_speed.rs src/get_volume_of_cuboid.rs src/paperwork.rs src/update_light.rs
-
-/Users/leonardorossi/Desktop/projetos/codewars-rust/target/debug/deps/codewars_rust-5a4ffc11e18e4e7f.d: src/main.rs src/cockroach_speed.rs src/get_volume_of_cuboid.rs src/paperwork.rs src/update_light.rs
-
-src/main.rs:
-src/cockroach_speed.rs:
-src/get_volume_of_cuboid.rs:
-src/paperwork.rs:
-src/update_light.rs:
